f29486a20a3f70b7559412502fb6c9950e43c770
Talia Seada <talias1235@gmail.com>
2022-05-22 23:08:52 +0300

    finnish proto roommate

--- building.py
+++ building.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 
 # read the data frame
+import roomatesOrApartments
+
 buildings = pd.read_csv(r'building_updated.csv')
 buildings.drop("Unnamed: 0", inplace=True, axis=1)
 # print(buildings.columns)
@@ -16,5 +18,26 @@ def addRow(gas=None, livingroom=None, bedroom=None, bathroom=None, city=None, wh
                                            furnished, AC, accesiable, elevator, parking, pets, smoking, price, length,
                                            phone, name]
 
-# def find_tenants():
-#
+def find_tenants(rowindex):
+    apartment_row= buildings.iloc[rowindex]
+    for index, rows in roomatesOrApartments.data.iterrow():
+        print()
+
+        #bool values
+        # electric/gas
+        # acsesiable
+        #loud
+        #construction
+        #elevator
+        #storage
+        #livingroom
+        #balcony/garden
+        #smoke
+        #pets
+        #city
+        #house/building
+        #furnished
+        #
+
+
+
